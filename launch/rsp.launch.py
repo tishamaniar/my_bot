01f6b81e125ea102f22cd5a63e0e45dd653f44c2
tishamaniar <tisha.maniar@gmail.com>
2026-06-02 13:40:12 +0530

    Refactor launch file to use robot.urdf.xacro

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -12,30 +12,39 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot_core.xacro')
-    robot_description_config = xacro.process_file(xacro_file).toxml()
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    pkg_path = os.path.join(
+        get_package_share_directory('my_bot')
+    )
+xacro_file = os.path.join(
+        pkg_path,
+        'description',
+        'robot.urdf.xacro'
+    )
+
+    robot_description_config = xacro.process_file(
+        xacro_file
+    ).toxml()
+
+    params = {
+        'robot_description': robot_description_config,
+        'use_sim_time': use_sim_time
+    }
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
-    )
+ )
 
-
-    # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
+            description='Use sim time if true'
+        ),
         node_robot_state_publisher
     ])
+
